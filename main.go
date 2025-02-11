4c924a78f516ce291a58b0263065d488aab0440f
divyansh <divyansh2375@gmail.com>
2025-02-11 16:52:45 +0530

    Added all 5 methods and server running

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+import (
+
+	"fmt"
+	"net/http"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+// struct is like object 
+//we'll use slice 
+
+type Movie struct {
+	ID string "json:id"
+	Isbn string "json:isbn"
+	Title string "json:title"
+	Director *Director "json:director"
+
+}
+
+type Director struct {
+	Firstname string "json:firstname"
+	Lasttname string "json:lastname"
+}
+
+var movies []Movie
+
+
+
+
+func main(){
+
+	r := mux.NewRouter()
+	r.HandleFunc("/movies" , getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}" , getMovie).Methods("GET")
+	r.HandleFunc("/movies" , createMovie).Methods("POST")
+	r.HandleFunc("movies/{id}" , updateMovie).Methods("PUT")
+	r.HandleFunc("movies/{id}" , deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Server is runnin on port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000" , r))
+
+}
